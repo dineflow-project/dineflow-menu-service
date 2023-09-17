9d681ea43180800b8893a369055011f34ec583fd
Akkharawat <akkharawatbct@gmail.com>
2023-09-17 13:19:59 +0700

    Remove the ignored directory "some-directory"

--- database/canteens.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `canteens`
-(
-    id   bigint auto_increment,
-    name varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
-
-INSERT INTO `canteens` (`name`) VALUES ('iCanteen'), ('Commarts');

4a81efc03607a2d71389f0401340ba35e29aba5c
AunchisaS <creamaunchisa09@gmail.com>
2023-11-14 00:35:37 +0700

    edit db

--- database/canteens.sql
+++ database/canteens.sql
@@ -9,8 +9,20 @@ INSERT INTO
     `canteens` (name, image_path)
 VALUES (
         'iCanteen',
-        '/images/canteen_item01.jpg'
+        'https://media.discordapp.net/attachments/1171496314807267378/1173531957066485771/2iCatneen-P2.jpg?ex=65644bcd&is=6551d6cd&hm=031a7afd179862b56a37ba2b231a54f86c732d8370967cdeabf7bfc32c836c8d&=&width=600&height=600'
     ), (
         'Commarts',
-        '/images/canteen_item02.jpg'
+        'https://media.discordapp.net/attachments/1171496314807267378/1173531956596703303/Aksorn.jpg?ex=65644bcd&is=6551d6cd&hm=b8a52e6b3bd7f3424d399c23981d64533f948e0811811139f59a5f7425ce9e42&=&width=662&height=662'
+    ),(
+        'PolSci',
+        'https://media.discordapp.net/attachments/1171496314807267378/1173531955879481374/PolSci.jpg?ex=65644bcd&is=6551d6cd&hm=d60b2cb7f380086e9f0c598397b0ec37c33c57f1e757f39ef5ec07bc93242b14&=&width=662&height=662'
+    ), (
+        'Medicine',
+        'https://media.discordapp.net/attachments/1171496314807267378/1173531954939973712/DoctorCanteen.jpg?ex=65644bcd&is=6551d6cd&hm=25a1749b95a1785047e746f7853d8dbf25bbe0fa26c926c2df979bcc78955f36&=&width=662&height=662'
+    ), (
+        'FoodHubb',
+        'https://media.discordapp.net/attachments/1171496314807267378/1173531954382114836/FoodHubb.jpg?ex=65644bcd&is=6551d6cd&hm=32ff1868e5062f125e32e72d4b0cf015740436b6604fe4037e7ec661ab7cbb5a&=&width=662&height=662'
+    ),(
+        'FoodWorld',
+        'https://media.discordapp.net/attachments/1171496314807267378/1173531955363577976/FoodWorld.jpg?ex=65644bcd&is=6551d6cd&hm=3ad7cd3aadfd038cdd2e4a9ecbb21e622167bfd01e1a5c7ce0fc7a741dda30b5&=&width=662&height=662'
     );
--- database/vendors.sql
+++ database/vendors.sql
@@ -27,13 +27,13 @@ VALUES (
         '08:00',
         '18:00',
         'Open',
-        '/images/vendor_item01.jpg'
+        'https://media.discordapp.net/attachments/1171496314807267378/1173528248215416952/chu5602.png?ex=65644859&is=6551d359&hm=dd5f2b2e1c0c519a0a674c97102f69d9ccc490483a1d2c0a7d6742a823c20153'
     ), (
         2,
         'VendorTwo',
-        'asdgfasdg31a5efg6e6',
+        'asdgfasdg3185fg6e6',
         '09:30',
         '19:30',
         'Close',
-        '/images/vendor_item02.jpg'
+        'https://media.discordapp.net/attachments/1171496314807267378/1173528664038707240/chu5604.png?ex=656448bc&is=6551d3bc&hm=9ad29e61367299bc0a38bc07b2378e4e9f0fd1648f157d6e453fcfb107250aca'
     );
